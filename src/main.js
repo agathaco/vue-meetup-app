e1a1a0506257f71b5f188fba8c098a2322c6503b
Agathe Cocco <agathecocco@gmail.com>
2018-05-15 13:24:45 +0100

    built toolnav and left menu

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue';
-import App from './App';
-import router from './router';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
+import colors from 'vuetify/es5/util/colors';
+import App from './App';
+import router from './router';
 
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.red.darken2,
+    secondary: colors.grey.lighten1,
+    accent: colors.red.accent2,
+    error: colors.red.accent4,
+    info: colors.blue.accent2,
+    success: colors.green.lighten2,
+    warning: colors.amber.darken2,
+  },
+});
 
 Vue.config.productionTip = false;
 
